daad114ead37993beddde6a863aed6db1dfc16c3
Jimmer-Ball <jimmer.ball@gmail.com>
2023-01-11 15:30:56 +0000

    Added optional decimal precision and added square root function too

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,6 +1,9 @@
 package calculator
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
 // Add takes two numbers and returns the result of adding
 // them together.
@@ -24,19 +27,42 @@ func Multiply(a, b float64) float64 {
 	return val
 }
 
-// Divide takes two numbers a and b, and
-// returns the result of dividing a by b
-// Divide by zero is not allowed
+// Divide takes two numbers a and b, and optional precision
+// and return the result of dividing a by b to the precision
+// if provided. Note that divide by zero is not allowed
 //
 // See https://go.dev/doc/faq#exceptions about why GO
 // does not have exceptions
-func Divide(a, b float64) (float64, error) {
+func Divide(a float64, b float64, precision ...uint) (float64, error) {
 	var val float64 = 0
 	var err error = nil
 	if b == 0 {
 		err = errors.New("cannot divide by zero")
 	} else {
 		val = a / b
+		if len(precision) == 1 {
+			val = roundValue(val, precision[0])
+		}
 	}
 	return val, err
+}
+
+func Sqrt(a float64, precision ...uint) (float64, error) {
+	var result float64 = 0
+	var err error = nil
+	if a < 0 {
+		err = errors.New("cannot square root a negative number")
+	} else {
+		result = math.Sqrt(a)
+		if len(precision) == 1 {
+			result = roundValue(result, precision[0])
+		}
+	}
+	return result, err
+}
+
+// See https://gosamples.dev/round-float/ for details
+func roundValue(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
 }
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -2,7 +2,6 @@ package calculator_test
 
 import (
 	"calculator"
-	"math"
 	"testing"
 )
 
@@ -15,11 +14,12 @@ type testCase struct {
 }
 
 func TestAdd(t *testing.T) {
-	// So any test, and any test case within each test can be run at
-	// the same time, speeds up running the overall collection of tests
+	// Allows any test, and any test case within each test to be run at
+	// the same time. Speeds up running the overall collection of tests
 	// within the test file hugely. Unfortunately within IntelliJ, the
-	//GO plugin doesn't count test cases, just test methods, so you loose
-	// visibility of the overall number of tests if you use test cases
+	// GO plugin doesn't count test cases, just test functions, so you
+	// loose visibility of the overall number of actual tests run when
+	// you use test cases
 	t.Parallel()
 
 	testCases := []testCase{
@@ -31,6 +31,7 @@ func TestAdd(t *testing.T) {
 
 	// For each item in slice, get both the index and element, see
 	// https://gobyexample.com/range, and ignore the index variable
+	// using the special blank identifier "_"
 	for _, tc := range testCases {
 		got := calculator.Add(tc.a, tc.b)
 		if tc.want != got {
@@ -76,34 +77,83 @@ func TestMultiply(t *testing.T) {
 func TestDivide(t *testing.T) {
 	t.Parallel()
 
-	/*
-	 * Division by anything other than zero is allowed
-	 */
-	allowedTestCases := []testCase{
+	testCases := []testCase{
 		{a: 10, b: 2, want: 5},
 		{a: -10, b: 2, want: -5},
 		{a: 10, b: -2, want: -5},
+		{a: 0, b: 5, want: 0},
 	}
-	for _, tc := range allowedTestCases {
+	for _, tc := range testCases {
 		got, _ := calculator.Divide(tc.a, tc.b)
 		if tc.want != got {
 			t.Errorf("Divide(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
 		}
 	}
+}
 
-	/*
-	 * Divide by zero is invalid or not allowed
-	 */
-	invalidTestCases := []testCase{
-		{a: 1, b: 0, want: math.Inf(1)},
-		{a: -1, b: 0, want: math.Inf(-1)},
+func TestDivideByZero(t *testing.T) {
+	t.Parallel()
+
+	testCases := []testCase{
+		{a: 1, b: 0},
+		{a: -1, b: 0},
 	}
-	for _, tc := range invalidTestCases {
+	for _, tc := range testCases {
 		_, err := calculator.Divide(tc.a, tc.b)
 		if err != nil {
+			// We expected an error
 			t.Log(err)
 		} else {
-			t.Errorf("Divide(%f, %f): We expected an error", tc.a, tc.b)
+			t.Errorf("Divide(%f, %f): Failed to provide expected error", tc.a, tc.b)
 		}
+	}
+}
+
+func TestDivideAndRound(t *testing.T) {
+	t.Parallel()
+
+	var a float64 = 1
+	var b float64 = 3
+	var precision uint = 6
+	var want = 0.333333
+	got, _ := calculator.Divide(a, b, precision)
+	if want != got {
+		t.Errorf("DivideAndRound(%f, %f, %d): want %f, got %f", a, b, precision, want, got)
+	}
+}
+
+func TestSqrt(t *testing.T) {
+	t.Parallel()
+
+	var a float64 = 9
+	var want float64 = 3
+	got, _ := calculator.Sqrt(a)
+	if want != got {
+		t.Errorf("Sqrt(%f): want %f, got %f", a, want, got)
+	}
+}
+
+func TestSqrtNegative(t *testing.T) {
+	t.Parallel()
+
+	var a float64 = -9
+	_, err := calculator.Sqrt(a)
+	if err != nil {
+		// We expected an error
+		t.Log(err)
+	} else {
+		t.Errorf("Sqrt(%f):  Failed to provide expected error", a)
+	}
+}
+
+func TestSqrtAndRound(t *testing.T) {
+	t.Parallel()
+
+	var a float64 = 12
+	var want = 3.464102
+	var precision uint = 6
+	got, _ := calculator.Sqrt(a, precision)
+	if want != got {
+		t.Errorf("Sqrt(%f): want %f, got %f", a, want, got)
 	}
 }
