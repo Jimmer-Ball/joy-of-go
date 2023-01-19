8e0f77539b1ec31fd21110c86d3181481830e328
Jimmer-Ball <jimmer.ball@gmail.com>
2023-01-19 14:41:46 +0000

    Added external dependency on google go-cmp module using the command: go get "github.com/google/go-cmp/cmp" -t

--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -2,6 +2,7 @@ package bookstore_test
 
 import (
 	"bookstore"
+	"github.com/google/go-cmp/cmp"
 	"testing"
 )
 
@@ -33,32 +34,45 @@ func TestBuyNoneAvailable(t *testing.T) {
 // Getting all books returns a list of 3 books provided by the store
 func TestGetAll(t *testing.T) {
 	t.Parallel()
-	// books is an unlimited slice variable that can be used to reference an
+	// Expected
+	want := []bookstore.Book{
+		{Title: "Example Book", Author: "Dave Normal", Copies: 3},
+		{Title: "For the Love of Go", Author: "John Arundel", Copies: 4},
+		{Title: "Get me my gun", Author: "Raul Fandango"},
+	}
+
+	// got is an unlimited slice variable that can be used to reference an
 	// underlying array of bookstore.Book items. See https://go.dev/doc/effective_go#slices
-	var books []bookstore.Book
-	books, _ = bookstore.GetAll()
-	if books != nil {
-		count := len(books)
+	var got []bookstore.Book
+	got, _ = bookstore.GetAll()
+	if got != nil {
+		count := len(got)
+		// Use the deep compare provided by the "cmp" module
+		// instead of rolling our own with reflect.DeepEqual
+		if !cmp.Equal(want, got) {
+			// What is the difference between want and got
+			t.Errorf("Not what was wanted:\n %s", cmp.Diff(want, got))
+		}
 		if count == 3 {
-			t.Log("We got 3 books like we expected")
+			t.Log("We got 3 books like we wanted")
 
 			// Slices are a reference into an existing collection of things
-			if books[0].Title != "Example Book" &&
-				books[1].Title != "For the Love of Go" &&
-				books[2].Title != "Get me my gun" {
-				t.Error("Books not in the right order")
+			if got[0].Title != "Example Book" &&
+				got[1].Title != "For the Love of Go" &&
+				got[2].Title != "Get me my gun" {
+				t.Error("got books in wrong order")
 			}
 
 			// We can use a slice index to modify an array
-			books[0].Title = "Updated Example Book"
-			if books[0].Title != "Updated Example Book" {
+			got[0].Title = "Updated Example Book"
+			if got[0].Title != "Updated Example Book" {
 				t.Error("Something is wrong here")
 			}
 
 			// We can add to an array using a slice
-			books = append(books, bookstore.Book{Title: "Another book", Author: "Dave Normal", Copies: 2})
-			if len(books) != 4 {
-				t.Error("We appended to the books listing, but the expected length is wrong")
+			got = append(got, bookstore.Book{Title: "Another book", Author: "Dave Normal", Copies: 2})
+			if len(got) != 4 {
+				t.Error("We appended to the got books listing, but the expected length is wrong")
 			}
 		} else {
 			t.Errorf("We got the wrong number of books, size %d", count)
--- bookstore/go.mod
+++ bookstore/go.mod
@@ -1,3 +1,7 @@
 module bookstore
 
 go 1.19
+
+// See https://go.dev/doc/modules/gomod-ref for defining versions
+// of dependencies in a go.mod file
+require github.com/google/go-cmp v0.5.9
